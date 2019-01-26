ca93f5ea02e464b962a5ce7c0524fd3a138158bc
yanglhan <836564993@qq.com>
2019-01-26 10:18:29 +0800

    Initial Commit

---
+++ CNJVideoView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CNJVideoView
+//
+//  Created by etz on 2019/1/26.
+//  Copyright © 2019年 alibaba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CNJVideoView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CNJVideoView
+//
+//  Created by etz on 2019/1/26.
+//  Copyright © 2019年 alibaba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
